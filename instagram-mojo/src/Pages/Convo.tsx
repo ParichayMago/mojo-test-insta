8015dc847d9c55dbd88eed3d8beca76a66c72ea5
ParichayMago <parichaymago@gmail.com>
2025-03-01 17:03:27 +0530

    svg

--- instagram-mojo/src/Pages/Convo.tsx
+++ instagram-mojo/src/Pages/Convo.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Loading from "../Components/Loading";
+import { plusPing } from "../assets/icons";
 
 // Define types for conversation data
 interface Conversation {
@@ -190,18 +191,7 @@ const ConvoPage: React.FC = () => {
                     )}
                   </div>
                   <div className="bg-gray-700 p-2 rounded-full">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 text-purple-400"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    {plusPing}
                   </div>
                 </div>
                 {convo.message_count !== undefined && (
